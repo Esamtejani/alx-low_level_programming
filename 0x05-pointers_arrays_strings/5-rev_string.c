d7ac9807ccef628960124e567f2db4f2ff305e7f
Esamtejani <esamtegani2@gmail.com>
2023-09-18 07:29:47 -0700

    task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,13 @@ void rev_string(char *s)
 
 	char temp;
 
-	for (l = 0; s[l] != '\0' ; ++l )
+	for (l = 0; s[l] != '\0' ; ++l)
 		;
-	
 	/* this for */
 	for (i = 0; i < l / 2; i++)
 	{
-		temp=s[i];
+		temp = s[i];
 		s[i] = s[l - 1 - i];
 		s[l - 1 - i] = temp;
-		
 	}
 }
