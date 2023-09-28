452c3de4504f739a8f7f267a1849f5a61d2ef07e
Esamtejani <esamtegani2@gmail.com>
2023-09-28 09:05:01 +0300

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
  * Return:if there is  success 1.
  * if there is an error , -1 is returned, and errno is set appropriately.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	return (0);
 }
