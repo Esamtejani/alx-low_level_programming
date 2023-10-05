4e86fd3d7f9d356c6bd9776accfaf0393c93fb3a
Esamtejani <esamtegani2@gmail.com>
2023-10-05 13:15:32 +0300

    tsak3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,36 @@
 #include "notrebloh.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * **alloc_grid -is a  function to allocate memory to grid
- * @width:the  int type
- * @height:the  int type
- * Return:the  grid of 0s
+ * *array_range - make  memory for an array
+ * @min: int type
+ * @max: int type
+ * Return:  return pointer to array
  */
-int **alloc_grid(int width, int height)
-{
-	int x, y;
-	int **ptr;
 
-	if (width <= 0 || height <= 0)
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int arr;
+
+	if (min > max)
 	{
-		return  (NULL);
+		return (NULL);
 	}
-	ptr = malloc(height * sizeof(int *));
+
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (x = 0; x < height; x++)
+	arr = 0;
+	while (min <= max)
 	{
-		ptr[x] = malloc(width * sizeof(int));
-		if (ptr[x] == NULL)
-		{
-			for (y = 0; y < x;  y++)
-				free(ptr[y]);
-			free(ptr);
-			return (NULL);
-		}
-		for (y = 0; y < width; y++)
-		{
-			ptr[x][y] = 0;
-		}
+		ptr[arr] = min;
+		min++;
+		arr++;
 	}
 	return (ptr);
 }
