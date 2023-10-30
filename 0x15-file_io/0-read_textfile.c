0368cbc936de02e1b30445a067c17386e73815bb
Esamtejani <esamtegani2@gmail.com>
2023-10-30 18:38:50 +0300

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,66 +3,39 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a specified number of letters from a text file
- * @filename: pointer to the name of the file
- * @letters: number of letters to read from the file
- *
- * Return: number of letters read and printed, or 0 if an error occurred
+ * read_textfile - it that reads a text file and prints
+ * @filename: it is a variable pointer
+ * @letters: it is a size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	struct stat st;
-	ssize_t file, lett, e;
+	ssize_t file, lett, ww;
 	char *text;
 
-	if (filename == NULL)
-	{
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
-	}
+
+	if (filename == NULL)
+		return (0);
 
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
 	{
-		return (0);
-	}
-
-	if (fstat(file, &st) == -1)
-	{
-		close(file);
-		return (0);
-	}
-
-	lett = st.st_size;
-	if (lett > letters)
-	{
-		lett = letters;
-	}
-
-	text = malloc(lett);
-	if (text == NULL)
-	{
-		close(file);
-		return (0);
-	}
-
-	lett = read(file, text, let);
-	if (lett == -1)
-	{
 		free(text);
-		close(file);
 		return (0);
 	}
 
-	e = write(STDOUT_FILENO, text, lett);
-	if (e != let)
-	{
-		free(text);
-		close(file);
-		return (0);
-	}
+	lett = read(file, text, letters);
 
-	free(text);
+	ww = write(STDOUT_FILENO, text, let);
+
 	close(file);
-	return (e);
-}
 
+	return (ww);
+}
